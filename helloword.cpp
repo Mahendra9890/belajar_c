946603c1262792d56872ada77611b4d50a652e11
prabski <prabumahendra255@gmail.com>
2024-09-12 18:06:29 +0700

    ini penting

---
+++ helloword.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main()
+{
+    // angka
+    int age = 17;
+    int year = 2024;
+    int days = 7.5;
+
+    // double(number including decimal)
+    double price = 10.99;
+    double gpa = 9.88;
+
+    // singgel character
+    char grade = 'p';
+
+    // boolean(true or false)
+    bool murid = true;
+    bool power = false;
+    bool sale = true;
+
+    // string(text)
+    std::string prabu = "bro";
+    std::string food = "nasi babi guling";
+    std::string rumah = "bali";
+
+    std::cout << "prabu " << prabu << "\n";
+    std::cout << "makanan fav " << food << " rumah"
+              << "\n";
+
+    return 0;
+}
